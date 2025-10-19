7cf756d0c7ae65a802681ac58f98659cc134dbce
Maku Serrano <maku_serrano@outlook.com>
2025-10-19 09:06:16 -0300

    termine de crear el metodo Leer Tarea

--- src/Repositories/CSVManager.java
+++ src/Repositories/CSVManager.java
@@ -2,17 +2,20 @@ package Repositories;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
+import Enums.Estado;
+import Enums.Prioridad;
 import Model.Tarea;
 
 public class CSVManager {
 
     private final String rutaArchivo;
-    private static final String HEADER = "id, titulo, descripcion, fecha_inicio, fecha_vencimiento, estado, prioridad";
+    private static final String HEADER = "id,titulo,descripcion,fecha_inicio,fecha_vencimiento,estado,prioridad";
 
     public CSVManager(String rutaArchivo) {
         this.rutaArchivo = rutaArchivo;
@@ -26,19 +29,31 @@ public class CSVManager {
             System.out.println("El archivo se creara al guardar.");
             return tareas;
         }
-        try(BufferedReader br = new BufferedReader(new FileWriter(file))){
+        try (BufferedReader br = new BufferedReader(new FileReader(file))) {
             String linea = br.readLine();
-            while((linea = br.readLine()) != null){
-                if(linea.trim().isEmpty())continue;
-                String[] campos = linea.split(",");
-                if (campos.length != 7) continue;
-                long id = Long.parseLong(campos[0]);
-                String titulo = campos[1].replace("\"", "");
-                String descripcion = campos[2].replace("\"", "");
-                LocalDate fecha_inicio = LocalDate.parse(campos[3]);
-                LocalDate fecha_vencimiento = LocalDate.parse(campos[4]);
-                Estado estado = 
 
+            while ((linea = br.readLine()) != null) {
+                if (linea.trim().isEmpty())
+                    continue;
+
+                String[] campos = linea.split(",");
+                if (campos.length != 7)
+                    continue;
+
+                long id = Long.parseLong(campos[0].trim());
+                String titulo = campos[1].replace("\"", "").trim();
+                String descripcion = campos[2].replace("\"", "").trim();
+                LocalDate fecha_inicio = LocalDate.parse(campos[3].trim());
+                LocalDate fecha_vencimiento = LocalDate.parse(campos[4].trim());
+                Estado estado = Estado.valueOf(campos[5].trim().toUpperCase());
+                Prioridad prioridad = Prioridad.valueOf(campos[6].trim().toUpperCase());
+
+                Tarea tarea = new Tarea(id, titulo, descripcion, fecha_inicio, fecha_vencimiento, estado, prioridad);
+                tareas.add(tarea);
+            }
+        } catch (Exception e) {
+            System.out.println("Error al leer el archivo" + e.getMessage());
         }
+        return tareas;
     }
 }
